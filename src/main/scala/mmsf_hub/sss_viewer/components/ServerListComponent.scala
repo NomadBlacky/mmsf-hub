abe9ac19d932e65ad5512cf5695233a65dcf3240
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-08-03 23:11:19 +0900

    Apply material-ui List and ListItem

--- src/main/scala/mmsf_hub/sss_viewer/components/ServerListComponent.scala
+++ src/main/scala/mmsf_hub/sss_viewer/components/ServerListComponent.scala
@@ -4,24 +4,22 @@ import mmsf_hub.sss_viewer.model.Server
 import slinky.core.StatelessComponent
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
-import slinky.web.html._
-
-import scala.scalajs.js
+import typings.materialUiCore.{components => Mui}
+import typings.react.mod.CSSProperties
 
 @react class ServerListComponent extends StatelessComponent {
   case class Props(servers: Seq[Server], selectedServerId: Int, onClick: Int => Unit)
 
   def render(): ReactElement =
-    ul(className := "server")(
+    Mui.List.className("server")(
       props.servers.map { server =>
         val bg = if (props.selectedServerId == server.id) "lightgreen" else "inherit"
-        li(
-          key := server.id.toString,
-          onClick := (() => props.onClick(server.id)),
-          style := js.Dynamic.literal(background = bg)
-        )(
-          s"Lv.${server.level}: ${server.name}"
-        )
+        Mui.ListItem
+          .withKey(server.id.toString)
+          .style(CSSProperties().setBackground(bg))
+          .onClick(_ => props.onClick(server.id))(
+            s"Lv.${server.level}: ${server.name}"
+          )
       }
     )
 
